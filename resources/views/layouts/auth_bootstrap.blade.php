2fcf4ce1f58ef5bf46de005d39c017267c687924
Yonathan Castillo <leothan522@gmail.com>
2025-01-25 14:07:36 -0400

    Preloader

--- resources/views/layouts/auth_bootstrap.blade.php
+++ resources/views/layouts/auth_bootstrap.blade.php
@@ -35,6 +35,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap" rel="stylesheet">
 
     <style>
+        @media (min-width: 768px) {
+            #scale {
+                transform: scale(0.8); /* Reduce el tamaño al 80% */
+            }
+        }
         *{
             font-family: "Poppins", sans-serif;
             font-weight: 400;
@@ -70,6 +75,41 @@
             }
         }
 
+        #preloader {
+            position: fixed;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            background: #fff no-repeat center center;
+            z-index: 9999;
+        }
+
+        #preloader::before {
+            content: "";
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            width: 100px;
+            height: 100px;
+            background: url('{{ asset('img/preloader_171x171.png') }}') no-repeat center center;
+            background-size: contain;
+            transform: translate(-50%, -50%);
+            animation: pulse 2s infinite;
+        }
+
+        @keyframes pulse {
+            0% {
+                transform: translate(-50%, -50%) scale(1);
+            }
+            50% {
+                transform: translate(-50%, -50%) scale(1.2);
+            }
+            100% {
+                transform: translate(-50%, -50%) scale(1);
+            }
+        }
+
     </style>
 
     @livewireStyles
@@ -77,11 +117,21 @@
 </head>
 <body style="background-color: #eee;">
 
+<div id="preloader"></div>
+
 @yield('content')
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 @livewireScripts
 <script type="application/javascript">
+
+    //Script para ejecurar el preloader
+    window.addEventListener('load', function() {
+        document.querySelector('#preloader').style.display = 'none';
+        document.querySelector('.container').style.display = 'block';
+    });
+
+    //Validacion Formularios
     (() => {
         'use strict'
 
@@ -102,6 +152,7 @@
             }, false)
         })
     })();
+
 </script>
 @yield('js')
 </body>
