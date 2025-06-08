8a6021a6421da6949bae6eb46b5543294a6daf4f
Yonathan Castillo <leothan522@gmail.com>
2025-06-07 21:07:28 -0400

    Alerta de export Pagos

--- app/Livewire/Dashboard/PagosReportesComponent.php
+++ app/Livewire/Dashboard/PagosReportesComponent.php
@@ -43,8 +43,31 @@ class PagosReportesComponent extends Component
             $pagos = Pago::whereBetween('fecha', [$this->desde, $this->hasta])->where('metodo', $this->filtro)->orderBy('fecha')->get();
         }
 
-        $this->toastBootstrap('info', 'Descarga Iniciada.');
-        return Excel::download(new PagosExport($pagos), "Pagos_$name.xlsx");
+        if ($pagos->isNotEmpty()){
+            $this->toastBootstrap('info', 'Descarga Iniciada.');
+            return Excel::download(new PagosExport($pagos), "Pagos_$name.xlsx");
+        }else{
+            $html = '
+                    <div class="row">
+                        <div class="col-12 p-2">
+                            <div class="small-box" style="box-shadow: none; min-height: 40px;">
+                                <div class="overlay bg-light">
+                                    <i class="far fa-4x fa-lightbulb opacity-75 text-warning"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 text-justify">
+                            No existen pagos registrados entre el rango de fechas que has indicado.
+                        </div>
+                    </div>
+                ';
+
+            $this->htmlToastBoostrap(null, null, [
+                'type' => 'warning',
+                'message' => $html
+            ]);
+            return false;
+        }
     }
 
 }
