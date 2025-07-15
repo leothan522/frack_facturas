ab6012f5516483493c7a97533a6cb06ee7a2ca55
Yonathan Castillo <leothan522@gmail.com>
2025-07-14 21:21:57 -0400

    Correcion spinner Auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,13 @@
 
     <div class="text-center pt-1 mb-5 pb-1 position-relative">
         @auth
-            <a class="text-muted" href="{{ route('profile.show') }}" onclick="verCargando()">{{ __('Profile') }}</a>
-            <a class="text-muted ms-3" href="{{ url('/dashboard') }}" onclick="verCargando()">Dashboard</a>
+            <a class="text-muted" href="{{ route('profile.show') }}" onclick="verCargandoAuth(this)">{{ __('Profile') }}</a>
+            <a class="text-muted ms-3" href="{{ url('/dashboard') }}" onclick="verCargandoAuth(this)">Dashboard</a>
         @else
-            <a class="text-muted me-3" href="{{ route('web.consultar') }}" onclick="verCargando()">Consultar tu cuenta</a>
-            <a class="text-muted" href="{{ route('login') }}" onclick="verCargando()">{{ __('Log in') }}</a>
+            <a class="text-muted me-3" href="{{ route('web.consultar') }}" onclick="verCargandoAuth(this)">Consultar tu cuenta</a>
+            <a class="text-muted" href="{{ route('login') }}" onclick="verCargandoAuth(this)">{{ __('Log in') }}</a>
             @if (Route::has('register'))
-                <a class="text-muted ms-3" href="{{ route('register') }}" onclick="verCargando()">{{ __('Register') }}</a>
+                <a class="text-muted ms-3" href="{{ route('register') }}" onclick="verCargandoAuth(this)">{{ __('Register') }}</a>
             @endif
         @endauth
         <div class="position-absolute top-50 start-50 translate-middle d-none verCargando">
